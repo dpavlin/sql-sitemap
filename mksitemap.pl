7e28ac7358141047e963959081a2f547ffb54ba1
Dobrica Pavlinusic <dpavlin@rot13.org>
2011-02-19 15:47:30 +0100

    limit number of urls in each sitemap to 32000

--- mksitemap.pl
+++ mksitemap.pl
@@ -31,22 +31,26 @@ warn "got $rows biblio items\n";
 
 my $fh;
 my $sitemap_nr = 0;
+my $num_urls = 0;
 
 while (my $row = $sth->fetchrow_hashref) {
 
 	if ( ! $fh ) {
 		$sitemap_nr++;
-		my $path = "sitemap$sitemap_nr.xml";
+		my $path = "out/sitemap$sitemap_nr.xml";
 		open($fh, '>', $path) || die "$path: $!";
 		warn "# $path created";
 
 		print $fh qq|<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n|;
+		$num_urls = 0;
 	}
 
 	print $fh qq|<url><loc>http://koha.ffzg.hr/cgi-bin/koha/opac-detail.pl?biblionumber=$row->{biblionumber}</loc></url>\n|;
+	$num_urls++;
 
-	if ( -s $fh > 8 * 1024 * 1024 ) {
-		warn "# closing $sitemap_nr with ", -s $fh, " bytes";
+	# standard limit to 10 mb uncompressed and 50000 urls
+	if ( -s $fh > 8 * 1024 * 1024 || $num_urls == 32000 ) {
+		warn "# closing $sitemap_nr with ", -s $fh, " bytes and $num_urls urls";
 		print $fh qq{</urlset>\n};
 		close($fh);
 		undef $fh;
@@ -56,5 +60,5 @@ while (my $row = $sth->fetchrow_hashref) {
 
 print $fh qq{</urlset>\n};
 close($fh);
-warn "# last $sitemap_nr with ", -s $fh, " bytes";
+warn "# closing $sitemap_nr with ", -s $fh, " bytes and $num_urls urls";
 
