4089aa71a41ae496148398944fcaab3650b127b7
Dobrica Pavlinusic <dpavlin@rot13.org>
2011-02-20 15:27:30 +0100

    example of grant for sitemap user

--- sql/koha.sql
+++ sql/koha.sql
@@ -1,6 +1,9 @@
 -- DBI:mysql:dbname=koha
 -- user:sitemap
 
+-- you need to grant priviledges to this user using something like:
+-- mysql> grant select on koha.biblio to 'sitemap'@'localhost';
+
 	select
 		concat('http://koha.ffzg.hr/cgi-bin/koha/opac-detail.pl?biblionumber=',biblionumber) as loc,
 		date(timestamp) as lastmod
